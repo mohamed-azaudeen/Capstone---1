3a82be4e84f6eb925c14bc968652a0cf4196c68d
MOHAMED AZARUDEEN <azarudeen48@gmail.com>
2024-05-11 00:30:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -379,136 +379,163 @@ def show_comments_table():
     df2 = st.dataframe(cm_details)
     
     return df2
+def page_home():
+    st.title(":red[YOUTUBE DATA HARVESTING & WAREHOUSING]")
 
-st.title(":red[YOUTUBE DATA HARVESTING & WAREHOUSING]")
 
+    channel_id=st.text_input("Enter the channel ID")
 
-channel_id=st.text_input("Enter the channel ID")
+    if st.button("Collect & Store Datas"):
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client['Project_1']
+        col1=db["channel_information"]
 
-if st.button("Collect & Store Datas"):
+        ch_ids  = []
+        for data in col1.find({},{"_id":0,"channel_information":1}):
+            ch_ids.append(data["channel_information"]["channel_id"])
+
+        if channel_id in ch_ids:
+            st.success("Channel Details of the given Channel ID is already Exists")
+        else:
+            insert= channel_details(channel_id)
+            st.success(insert)
+
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client['Project_1']
     col1=db["channel_information"]
-
-    ch_ids  = []
+    all_channels=[]
     for data in col1.find({},{"_id":0,"channel_information":1}):
-        ch_ids.append(data["channel_information"]["channel_id"])
+        all_channels.append(data['channel_information']['channel_name'])
 
-    if channel_id in ch_ids:
-        st.success("Channel Details of the given Channel ID is already Exists")
-    else:
-        insert= channel_details(channel_id)
-        st.success(insert)
+    unique_values=st.selectbox("Select the Channels",all_channels)
 
-client = pymongo.MongoClient("mongodb://localhost:27017/")
-db = client['Project_1']
-col1=db["channel_information"]
-all_channels=[]
-for data in col1.find({},{"_id":0,"channel_information":1}):
-    all_channels.append(data['channel_information']['channel_name'])
+    if st.button("Migrate to SQL"):
+        Table=tables(unique_values)
+        st.success(Table)
+        
+    show_table=st.radio("SELECT THE TABLE FOR VIEW",("Channels","Videos","Comments"))
 
-unique_values=st.selectbox("Select the Channels",all_channels)
+    if show_table=="Channels":
+        show_channels_table()
 
-if st.button("Migrate to SQL"):
-    Table=tables(unique_values)
-    st.success(Table)
+    elif show_table=="Videos":
+        show_videos_table()
+
+    elif show_table=="Comments":
+        show_comments_table()
+
+def page_queries():
+
+    st.title(":red[Select Your Queries:1234:]")
+
+    client = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="azarudeen1997",
+        database="Youtube_Data"
+    )   
+    cursor = client.cursor()
+
+    question=st.selectbox("Select Your Questions",("1. What are the names of all the videos and their corresponding channels?",
+                                                "2. Which channels have the most number of videos, and how many videos do they have?",
+                                                "3. What are the top 10 most viewed videos and their respective channels?",
+                                                "4. How many comments were made on each video, and what are their corresponding video names?",
+                                                "5. Which videos have the highest number of likes, and what are their corresponding channel names?",
+                                                "6. What is the total number of likes for each video, and what are their corresponding video names?",
+                                                "7. What is the total number of views for each channel, and what are their corresponding channel names?",
+                                                "8. What is the average duration of all videos in each channel, and what are their corresponding channel names?",
+                                                "9. Which videos have the highest number of comments, and what are their corresponding channel names?",
+                                                "10 . Which channels have the most number of subscription and what are their corresponding channel names and published date"))
+
+    if question=="1. What are the names of all the videos and their corresponding channels?":
+        query1="""select video_title ,channel_title from videos"""
+        cursor.execute(query1)
+        t1=cursor.fetchall()
+        df=pd.DataFrame(t1,columns=['Video Title','Channel title'])
+        st.write(df)                                               
+
+    elif question=="2. Which channels have the most number of videos, and how many videos do they have?":
+        col1,col2=st.columns(2)
+        with col1:
+            query2="""select channel_name , channel_videoCount from channels order by channel_videoCount DESC"""
+            cursor.execute(query2)
+            t2=cursor.fetchall()
+            df2=pd.DataFrame(t2,columns=['Channel Name','Total no.of.Videos'])
+            st.write(df2)
+        with col2:
+            query2="""select channel_name , channel_videoCount from channels order by channel_videoCount """
+            cursor.execute(query2)
+            t2=cursor.fetchall()
+            df2=pd.DataFrame(t2,columns=['Channel Name','Total no.of.Videos'])
+            st.bar_chart(df2)                                                 
+
+    elif question=="3. What are the top 10 most viewed videos and their respective channels?":
+        query3="""select view_count,channel_title,video_title from videos where view_count is not null order by view_count DESC limit 10 """
+        cursor.execute(query3)
+        t3=cursor.fetchall()
+        df3=pd.DataFrame(t3,columns=['Top 10 Views','Channel Name','Video Title'])
+        st.write(df3)                                              
+
+    elif question=="4. How many comments were made on each video, and what are their corresponding video names?":
+        query4="""select comment_count , video_title from videos where comment_count is not null """
+        cursor.execute(query4)
+        t4=cursor.fetchall()
+        df4=pd.DataFrame(t4,columns=['Total no.of.Comments','Video Title'])
+        st.write(df4)                                               
+
+    elif question=="5. Which videos have the highest number of likes, and what are their corresponding channel names?":
+        query5="""select like_count , video_title , channel_title from videos where like_count is not null order by like_count DESC"""
+        cursor.execute(query5)
+        t5=cursor.fetchall()
+        df5=pd.DataFrame(t5,columns=['Highest no.of.Likes','Video Title','Channel Name'])
+        st.write(df5)
+
+    elif question=="6. What is the total number of likes for each video, and what are their corresponding video names?":
+        query6="""select like_count , video_title  from videos where like_count is not null"""
+        cursor.execute(query6)
+        t6=cursor.fetchall()
+        df6=pd.DataFrame(t6,columns=['Total no.of.Likes','Video Title'])
+        st.write(df6)
+
+    elif question=="7. What is the total number of views for each channel, and what are their corresponding channel names?":
+        query7="""select channel_viewCount , channel_name  from channels where channel_viewCount is not null"""
+        cursor.execute(query7)
+        t7=cursor.fetchall()
+        df7=pd.DataFrame(t7,columns=['Total no.of.Views','Channel Name'])
+        st.write(df7)
+            
+
+
+    elif question=="8. What is the average duration of all videos in each channel, and what are their corresponding channel names?":
+        query8="""select duration,channel_title from videos """
+        cursor.execute(query8)
+        t8=cursor.fetchall()
+        df8=pd.DataFrame(t8,columns=['Average Duration','Channel Name'])
+        st.write(df8)
+
+    elif question=="9. Which videos have the highest number of comments, and what are their corresponding channel names?":
+        query9="""select comment_count , video_title ,  channel_title  from videos where  comment_count is not null order by comment_count DESC"""
+        cursor.execute(query9)
+        t9=cursor.fetchall()
+        df9=pd.DataFrame(t9,columns=['Highest no.of.comments','Video Title','Channel Name'])
+        st.write(df9)
+
+    elif question=="10 . Which channels have the most number of subscription and what are their corresponding channel names and published date":
+        query10="""select channel_name , channel_subscriptionCount ,channel_publishedOn from channels where channel_subscriptionCount is not null order by channel_subscriptionCount DESC """
+        cursor.execute(query10)
+        t10=cursor.fetchall()
+        df10=pd.DataFrame(t10,columns=['Channel Name','Highest no.of.Subscription','Published Date'])
+        st.write(df10)
+
+def main():
+    st.sidebar.title("Navigation")
+    page = st.sidebar.radio("Go to", ["Home", "Queries"])
+
+    if page == "Home":
+        page_home()
+    elif page == "Queries":
+        page_queries()
     
-show_table=st.radio("SELECT THE TABLE FOR VIEW",("Channels","Videos","Comments"))
+if __name__ == "__main__":
+    main()
 
-if show_table=="Channels":
-    show_channels_table()
-
-elif show_table=="Videos":
-    show_videos_table()
-
-elif show_table=="Comments":
-    show_comments_table()
-
-client = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="azarudeen1997",
-    database="Youtube_Data"
-)   
-cursor = client.cursor()
-
-question=st.selectbox("Select Your Questions",("1. What are the names of all the videos and their corresponding channels?",
-                                               "2. Which channels have the most number of videos, and how many videos do they have?",
-                                               "3. What are the top 10 most viewed videos and their respective channels?",
-                                               "4. How many comments were made on each video, and what are their corresponding video names?",
-                                               "5. Which videos have the highest number of likes, and what are their corresponding channel names?",
-                                               "6. What is the total number of likes for each video, and what are their corresponding video names?",
-                                               "7. What is the total number of views for each channel, and what are their corresponding channel names?",
-                                               "8. What is the average duration of all videos in each channel, and what are their corresponding channel names?",
-                                               "9. Which videos have the highest number of comments, and what are their corresponding channel names?",
-                                               "10 . Which channels have the most number of subscription and what are their corresponding channel names and published date"))
-
-if question=="1. What are the names of all the videos and their corresponding channels?":
-    query1="""select video_title ,channel_title from videos"""
-    cursor.execute(query1)
-    t1=cursor.fetchall()
-    df=pd.DataFrame(t1,columns=['Video Title','Channel title'])
-    st.write(df)                                               
-
-elif question=="2. Which channels have the most number of videos, and how many videos do they have?":
-    query2="""select channel_name , channel_videoCount from channels order by channel_videoCount DESC"""
-    cursor.execute(query2)
-    t2=cursor.fetchall()
-    df2=pd.DataFrame(t2,columns=['Channel Name','Total no.of.Videos'])
-    st.write(df2)                                               
-
-elif question=="3. What are the top 10 most viewed videos and their respective channels?":
-    query3="""select view_count,channel_title,video_title from videos where view_count is not null order by view_count DESC limit 10 """
-    cursor.execute(query3)
-    t3=cursor.fetchall()
-    df3=pd.DataFrame(t3,columns=['Top 10 Views','Channel Name','Video Title'])
-    st.write(df3)                                              
-
-elif question=="4. How many comments were made on each video, and what are their corresponding video names?":
-    query4="""select comment_count , video_title from videos where comment_count is not null """
-    cursor.execute(query4)
-    t4=cursor.fetchall()
-    df4=pd.DataFrame(t4,columns=['Total no.of.Comments','Video Title'])
-    st.write(df4)                                               
-
-elif question=="5. Which videos have the highest number of likes, and what are their corresponding channel names?":
-    query5="""select like_count , video_title , channel_title from videos where like_count is not null order by like_count DESC"""
-    cursor.execute(query5)
-    t5=cursor.fetchall()
-    df5=pd.DataFrame(t5,columns=['Highest no.of.Likes','Video Title','Channel Name'])
-    st.write(df5)
-
-elif question=="6. What is the total number of likes for each video, and what are their corresponding video names?":
-    query6="""select like_count , video_title  from videos where like_count is not null"""
-    cursor.execute(query6)
-    t6=cursor.fetchall()
-    df6=pd.DataFrame(t6,columns=['Total no.of.Likes','Video Title'])
-    st.write(df6)
-
-elif question=="7. What is the total number of views for each channel, and what are their corresponding channel names?":
-    query7="""select channel_viewCount , channel_name  from channels where channel_viewCount is not null"""
-    cursor.execute(query7)
-    t7=cursor.fetchall()
-    df7=pd.DataFrame(t7,columns=['Total no.of.Views','Channel Name'])
-    st.write(df7)
-
-elif question=="8. What is the average duration of all videos in each channel, and what are their corresponding channel names?":
-    query8="""select duration,channel_title from videos """
-    cursor.execute(query8)
-    t8=cursor.fetchall()
-    df8=pd.DataFrame(t8,columns=['Average Duration','Channel Name'])
-    st.write(df8)
-
-elif question=="9. Which videos have the highest number of comments, and what are their corresponding channel names?":
-    query9="""select comment_count , video_title ,  channel_title  from videos where  comment_count is not null order by comment_count DESC"""
-    cursor.execute(query9)
-    t9=cursor.fetchall()
-    df9=pd.DataFrame(t9,columns=['Highest no.of.comments','Video Title','Channel Name'])
-    st.write(df9)
-
-elif question=="10 . Which channels have the most number of subscription and what are their corresponding channel names and published date":
-    query10="""select channel_name , channel_subscriptionCount ,channel_publishedOn from channels where channel_subscriptionCount is not null order by channel_subscriptionCount DESC """
-    cursor.execute(query10)
-    t10=cursor.fetchall()
-    df10=pd.DataFrame(t10,columns=['Channel Name','Highest no.of.Subscription','Published Date'])
-    st.write(df10)
